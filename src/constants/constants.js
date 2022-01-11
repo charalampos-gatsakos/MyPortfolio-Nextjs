c50b70727271cfc3450587dd9bbdc385bdc90d75
Daddy-Gats <charalamposgatsakos@gmail.com>
2022-01-11 20:40:26 +0200

    Constants Edit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -24,5 +24,5 @@ export const TimeLineData = [
   { year: 2018, text: 'Worked as a freelance developer', },
   { year: 2019, text: 'Founded JavaScript Mastery', },
   { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  
 ];
