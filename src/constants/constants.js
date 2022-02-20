5330d02dae6565a2081ccbb0d3952dded16c33b7
Daddy-Gats <charalamposgatsakos@gmail.com>
2022-02-20 22:31:38 +0200

    Const Ud

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,7 +10,7 @@ export const projects = [
   },
   {
     title: 'Group Project (Dr.Builder) ',
-    description: "Using ASP.Net And Entity Framework We Created A FullStack Project With CRUD Functionality That Helps You Build Your Own PC Build With Dynamic Filters And Many More ...  ",
+    description: "Using ASP.Net And Entity Framework We Created A FullStack Project With CRUD Functionality That Helps You Build Your Own PC Build With Dynamic Filters And Many More ...",
       image: '/images/Dr.5.png',
       tags: ['ASP.Net', 'Entity Framework'],
     source: 'https://github.com/dimitriskaratzas/GroupProject',
